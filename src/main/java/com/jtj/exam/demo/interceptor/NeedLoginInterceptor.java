7dac923cd79a20b4e3abbd5b54c4a308378c8c5a
jtj3107 <jtj3926@gmail.com>
2021-09-14 11:35:41 +0900

    NeedLogin인터셉터가 여러 액션들을 대신해서 자격없는 요청들을 앞선에서 거름

--- src/main/java/com/jtj/exam/demo/interceptor/NeedLoginInterceptor.java
+++ src/main/java/com/jtj/exam/demo/interceptor/NeedLoginInterceptor.java
@@ -6,13 +6,19 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import com.jtj.exam.demo.vo.ResultData;
 import com.jtj.exam.demo.vo.Rq;
 
 @Component
 public class NeedLoginInterceptor implements HandlerInterceptor {
 	@Override
 	public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler) throws Exception {
-		System.out.println("로그인 필요");
+		Rq rq = (Rq)req.getAttribute("rq");
+		
+		if(!rq.isLogined()) {
+			rq.printHistoryBakJs("로그인 후 이용해주세요.");
+			return false;
+		}
 		
 		return HandlerInterceptor.super.preHandle(req, resp, handler);
 	}
