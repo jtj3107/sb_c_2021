4cf11a19b3b8730afd8755c14b05fab56aa47825
jtj3107 <jtj3926@gmail.com>
2021-09-17 11:38:52 +0900

    게시물리스트에 선택된 게시판에 관련된 게시물만 표시

--- src/main/java/com/jtj/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/jtj/exam/demo/controller/UsrArticleController.java
@@ -30,11 +30,15 @@ public class UsrArticleController {
 
 	@RequestMapping("/usr/article/list")
 	public String showList(HttpServletRequest req, Model model, int boardId) {
-		Board board = boardService.getBoardById(boardId);
-		
 		Rq rq = (Rq) req.getAttribute("rq");
 
-		List<Article> articles = articleService.getForPrintArticles(rq.getLoginedMemberId());
+		Board board = boardService.getBoardById(boardId);
+		
+		if(board == null) {
+			return rq.historyBackJsOnView(Ut.f("%d번 게시판은 존재하지 않습니다.", boardId));
+		}
+		
+		List<Article> articles = articleService.getForPrintArticles(rq.getLoginedMemberId(), boardId);
 
 		model.addAttribute("board", board);
 		model.addAttribute("articles", articles);
