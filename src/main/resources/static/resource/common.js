6abfe4215e5e2369111b0e14da2bc43dfe44698b
jtj3107 <jtj3926@gmail.com>
2021-10-04 18:15:13 +0900

    탑바 디자인, 검색기능 추가

--- src/main/resources/static/resource/common.js
+++ src/main/resources/static/resource/common.js
@@ -5,3 +5,37 @@ $('select[data-value]').each(function(index, el) {
 
 	$el.val(defaultValue);
 });
+
+/* 탑바 시작 */
+function TopBar__init() {  
+  $('.top-bar .btn-show-search-bar').click(function() {
+    SearchBar__show();
+  });
+}
+/* 탑바 끝 */
+
+/* 검색바 시작 */
+function SearchBar__init() {
+  $('.search-bar .btn-hide-search-bar').click(function() {
+    SearchBar__hide();
+  });
+}
+
+function SearchBar__show() {
+  $('html').addClass('search-bar-actived');
+  $('.search-bar').addClass('active');
+  
+  setTimeout(function() {
+	$('.search-bar form input[name="searchKeyword"]').focus();
+	}, 100);
+}
+
+function SearchBar__hide() {
+  $('html').removeClass('search-bar-actived');
+  $('.search-bar').removeClass('active');
+  $('.search-bar form input[name="searchKeyword"]').val('');
+}
+/* 검색바 끝 */
+
+TopBar__init();
+SearchBar__init();
